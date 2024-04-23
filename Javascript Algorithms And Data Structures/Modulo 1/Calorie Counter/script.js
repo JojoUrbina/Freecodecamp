60e64fff90d24a50ab624e4208a03961868c88ce
Jorman Urbina <jormanurbina1@gmail.com>
2024-04-23 10:51:59 +0200

    Updated input cleaning and validation functions
    - Updated cleanInputString() function to remove all characters except digits, plus signs, minus signs, and whitespace.
    - Added new function isInvalidInput() to validate if input contains a pattern of a number followed by 'e' and another number.
    - Added skeleton for addEntry() function.

--- Javascript Algorithms And Data Structures/Modulo 1/Calorie Counter/script.js
+++ Javascript Algorithms And Data Structures/Modulo 1/Calorie Counter/script.js
@@ -7,5 +7,15 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
-  const regex = /hello/;
+  const regex = /[+-\s]/g;
+  return str.replace(regex, '');
+}
+
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+
+function addEntry() {
+
 }
