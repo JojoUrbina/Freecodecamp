dad892d4a0acb7f91ffc6adfba20d0b0e6d59b3c
Jorman Urbina <jormanurbina1@gmail.com>
2024-04-27 11:58:11 +0200

    Agregar funciones para resaltar la canción actual y actualizar la visualización del reproductor
    Se agregaron las funciones `highlightCurrentSong` y `setPlayerDisplay` para resaltar la canción actual en la lista de reproducción y actualizar la visualización del reproductor con el título y el artista de la canción actual. Estas funciones se utilizan al reproducir una canción y al cambiar entre canciones.

--- Javascript Algorithms And Data Structures/Modulo 1/Music Player/script.js
+++ Javascript Algorithms And Data Structures/Modulo 1/Music Player/script.js
@@ -98,6 +98,8 @@ const playSong = (id) => {
   userData.currentSong = song;
   playButton.classList.add("playing");
 
+  highlightCurrentSong();
+  setPlayerDisplay();
   audio.play();
 };
 
@@ -129,13 +131,27 @@ const playPreviousSong = () => {
    }
 };
 
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
+};
+
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
   const songToHighlight = document.getElementById(
     `song-${userData?.currentSong?.id}`
   );
-  
 
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
+
+  if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
 };
 
 const renderSongs = (array) => {
@@ -159,6 +175,8 @@ const renderSongs = (array) => {
 
   playlistSongs.innerHTML = songsHTML;
 };
+
+
 
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
 
