2491c0b4ac6d191d4f113697737266e8766d1274
Jorman Urbina <jormanurbina1@gmail.com>
2024-04-29 16:11:00 +0200

    - Updated resetButton event listener to properly reset songs and UI. - Added event listener to handle the end of audio playback, providing logic for either playing the next song or resetting player state if no next song exists. - Ensured accessibility of play button text after song changes. - Applied missing newline at the end of the file for consistency.

--- Javascript Algorithms And Data Structures/Modulo 1/Music Player/script.js
+++ Javascript Algorithms And Data Structures/Modulo 1/Music Player/script.js
@@ -169,6 +169,9 @@ const deleteSong = (id) => {
     resetButton.addEventListener("click", () => {
       userData.songs = [...allSongs];
 
+      renderSongs(sortSongs()); 
+      setPlayButtonAccessibleText();
+      resetButton.remove();
     });
 
   }
@@ -247,6 +250,23 @@ previousButton.addEventListener("click", playPreviousSong);
 
 shuffleButton.addEventListener("click", shuffle);
 
+audio.addEventListener("ended", () => {
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
+
+    if (nextSongExists) {
+      playNextSong();
+    } else {
+      userData.currentSong = null;
+      userData.songCurrentTime = 0;  
+pauseSong();
+setPlayerDisplay();
+highlightCurrentSong()
+setPlayButtonAccessibleText()
+
+    }
+});
+
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
     if (a.title < b.title) {
@@ -264,3 +284,4 @@ const sortSongs = () => {
 };
 
 renderSongs(sortSongs());
+setPlayButtonAccessibleText();
