8d88e4901598842b0c2d990c28c3a2094e0f4c84
Jorman Urbina <jormanurbina1@gmail.com>
2024-04-30 13:04:03 +0200

    "Refactorización: Actualización del formato de fecha en el script.js del módulo 1 de Javascript Algorithms And Data Structures. Se agregaron opciones de formato de fecha 'yyyy-mm-dd' y 'mm-dd-yyyy-h-mm', con manejo adecuado de la selección del usuario."

--- Javascript Algorithms And Data Structures/Modulo 1/Data Formatter/script.js
+++ Javascript Algorithms And Data Structures/Modulo 1/Data Formatter/script.js
@@ -11,11 +11,20 @@ const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
-const exampleSentence = "selur pmaCedoCeerf".split("");
-console.log(exampleSentence);
 dateOptionsSelectElement.addEventListener("change", () => {
+
   switch (dateOptionsSelectElement.value) {
     case "yyyy-mm-dd":
       currentDateParagraph.textContent = formattedDate
+        .split("-")
+        .reverse()
+        .join("-");
+      break;
+    case "mm-dd-yyyy-h-mm":
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+      break;
+      default:
+      currentDateParagraph.textContent = formattedDate
+    
   }
 });
